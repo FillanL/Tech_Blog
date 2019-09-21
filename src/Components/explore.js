a71bb326c8945297804fa7bc647f8f08568482a4
Locallize <Fillanlee@aol.com>
2019-09-21 18:57:15 -0400

    explore page changes

--- src/Components/explore.js
+++ src/Components/explore.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react'
+
+import ExploreCard from '../Components/exploreCard'
 import '../CSS/explore.scss'
 
 export default class Explore extends Component {
+    state ={
+        toggle: {
+            exploreGrid: "25% 75%",
+            searchBox: ""
+        }
+    }
 
-    glide = ()=>{
-    //   document.getElementsByClassName('search-box').Style.backgroundColor = 'red'
-       console.log("here")
+    glide = () =>{
+        if(this.state.toggle.searchBox === ""){
+            return this.setState({
+                toggle:{
+                    exploreGrid: "100%",
+                    searchBox: "none"
+                }
+            })
+        } else{
+            return this.setState({
+                toggle:{
+                    exploreGrid: "25% 75%",
+                    searchBox: ""
+                }
+            })
+        }
     }
 
     render() {
+        const cards = [{card: 1},{card: 2}, {card: 2}]
         return (
-            <div className="explore">
-                <div className="search-box">
+            <div className="explore" style={{gridTemplateColumns: this.state.toggle.exploreGrid}}>
+                <div className="search-box" style={{display: this.state.toggle.searchBox}}>
                     <input type="search"
                     placeholder="what next"
                     />
@@ -20,7 +42,16 @@ export default class Explore extends Component {
                     </button>
                 </div>
                 <div className="card-area">
+                    <button onClick={()=>this.glide()}>
+                        Reduceeeit
+                    </button>
                     card area
+                    <div className="cards-container">
+                        {cards.map( card => 
+                            <ExploreCard key={card.card}/>
+
+                        )}    
+                    </div>
                 </div>
             </div>
         )
--- src/Components/exploreCard.js
+++ src/Components/exploreCard.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import '../CSS/explore.scss'
 
 export default class ExploreCard extends Component {
     render() {
         return (
-            <div>
-                <div className="feature-img">
+            <div className="card-box">
+                {/* <div className="feature-img">
 
                 <img srcSet="https://images.unsplash.com/photo-1557858310-9052820906f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="stuff" />
-                </div>
+                </div> */}
                 <div className="info-section">
                     <article>
                         <h3>Title</h3>
