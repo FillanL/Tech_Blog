76f7542559538dc8372ecdc6dec2000e558d5ea5
Locallize <Fillanlee@aol.com>
2020-03-10 04:31:43 -0400

    quil editor added and article create page fully designed

--- src/AdminComponents/newArticleForm.js
+++ src/AdminComponents/newArticleForm.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import './AdminCSS/newArticleForm.scss'
 
 function newArticleForm() {
@@ -9,18 +11,33 @@ function newArticleForm() {
                     back
                 </span>
             </div>
-            <form>
-                {/* <label>
-                    Article Title
-                </label><br></br> */}
-                <input type="text" placeholder="Title" /><br></br>
-                <label>Article feature Image</label><br></br>
-                <input type="file" /><br></br>
-                {/* <div>
-                    Content edittingg system
-                </div> */}
-                <button>Sumbit</button>
-            </form>
+            <div className='metaContainer'>
+                <input type="text" maxLength='80' placeholder="Article Title" /> 
+                <br></br>
+                <input type="file" style={{width:'auto'}} /><br></br>
+            </div>
+
+            <div className="editorContainer">
+                <ReactQuill
+                    style={{ backgroundColor: 'white' }}
+                    onChange={(x) => console.log(x)}
+                    theme='snow'
+                    placeholder={"Start typing heree..."}
+                    modules={{
+                        toolbar: [
+                            [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+                            [{ size: [] }],
+                            [{ 'align': ['', 'center', 'right', 'justify'] }],
+                            ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code'],
+                            [{ 'list': 'ordered' }, { 'list': 'bullet' },
+                            { 'indent': '-1' }, { 'indent': '+1' }, { 'align': 'center' }],
+                            ['link', 'image', 'video'],
+                            ['clean']
+                        ]
+                    }}
+                />
+                <button className="submitBtn">Submit</button>
+            </div>
         </div>
     )
 }
