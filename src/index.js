0f4412e10e6d43b60f47a4487b1817b5f3b6db8e
Locallize <Fillanlee@aol.com>
2020-04-21 00:28:36 -0400

    move back provider

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
+    <Provider store={store}>
+        <BrowserRouter>
             <App />
-    </Provider>
         </BrowserRouter>
+    </Provider>
 , document.getElementById('root'));
 
 serviceWorker.unregister();
